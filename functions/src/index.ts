1d2af1e1439fd5c35bb3ebbefc9cde49723d2b5c
mix5003 <mix5003@gmail.com>
2020-11-01 13:52:34 +0700

    try to add try catch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,19 +26,23 @@ const createReqtestFunction = (type:string, accountId: string, extractor: Functi
             const transaction = extractor(request.body.toString());
             console.log('Transaction', transaction);
             if(transaction){
-                createTransaction(accountId, transaction, db).then((entry) => {
-                    console.log('Entry', entry);
-                    collectionRef.doc(refId).set({
-                        text: request.body.toString(),
-                        type,
-                        entry,
-                    }).then(updated => {
-                        console.log('COMPLETED');
-                    }).catch(err => {
-                        console.log("Not Completed");
-                        console.error(err);
+                try{
+                    createTransaction(accountId, transaction, db).then((entry) => {
+                        console.log('Entry', entry);
+                        collectionRef.doc(refId).set({
+                            text: request.body.toString(),
+                            type,
+                            entry,
+                        }).then(updated => {
+                            console.log('COMPLETED');
+                        }).catch(err => {
+                            console.log("Not Completed");
+                            console.error(err);
+                        });
                     });
-                });
+                }catch(e){
+                    console.error(e);
+                }
             }
         });
     });
